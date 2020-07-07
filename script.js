b8979802129a50375efbaee43a4fd4235e9c9ce3
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-07-07 20:38:58 +0800

    Fixed the toggle button bug.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const toggleButton = document.querySelector(".toggle-button");
 const mobileNav = document.querySelector(".mobile-nav");
 const backdrop = document.getElementById("backdrop");
 
@@ -11,16 +10,19 @@ const openMobileNav = () => {
 const closeMobileNav = () => {
   mobileNav.style.display = "none";
   backdrop.style.display = "none";
+  document.body.style.overflow = "auto";
 };
 
 window.addEventListener("click", (event) => {
-  if (event.target.matches(".toggle-button")) {
+  if (event.target.matches("#toggle-button, .toggle-button__bar")) {
     openMobileNav();
   } else {
     closeMobileNav();
+    console.log("You haven't clicked anywhere.");
   }
 });
 
+// const toggleButton = document.querySelector(".toggle-button");
 // toggleButton.addEventListener("click", () => {
 //   mobileNav.classList.add("open");
 // });
