ea2d85e206365caecfe23102bd5c0d0735eb612e
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-07-07 19:09:27 +0800

    Added backdrop.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const toggleButton = document.querySelector(".toggle-button");
 const mobileNav = document.querySelector(".mobile-nav");
+const backdrop = document.getElementById("backdrop");
+
+const openMobileNav = () => {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+};
+
+const closeMobileNav = () => {
+  mobileNav.style.display = "none";
+  backdrop.style.display = "none";
+};
 
 window.addEventListener("click", (event) => {
   if (event.target.matches(".toggle-button")) {
-    mobileNav.style.display = "block";
+    openMobileNav();
   } else {
-    mobileNav.style.display = "none";
+    closeMobileNav();
   }
 });
 
