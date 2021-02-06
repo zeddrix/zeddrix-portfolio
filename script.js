92f753394db60e1625343def7e57081ceaa4a91e
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-02-06 13:58:47 +0800

    Mobile nav closes only when backdrop clicked.

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-const mobileNav = document.querySelector(".mobile-nav");
-const backdrop = document.getElementById("backdrop");
+const mobileNav = document.querySelector('.mobile-nav');
+const backdrop = document.getElementById('backdrop');
 
 const openMobileNav = () => {
-  mobileNav.style.display = "block";
-  backdrop.style.display = "block";
-  document.body.style.overflow = "hidden";
+	mobileNav.style.display = 'block';
+	backdrop.style.display = 'block';
+	document.body.style.overflow = 'hidden';
 };
 
 const closeMobileNav = () => {
-  mobileNav.style.display = "none";
-  backdrop.style.display = "none";
-  document.body.style.overflow = "auto";
+	mobileNav.style.display = 'none';
+	backdrop.style.display = 'none';
+	document.body.style.overflow = 'auto';
 };
 
-window.addEventListener("click", (event) => {
-  if (event.target.matches("#toggle-button, .toggle-button__bar")) {
-    openMobileNav();
-  } else {
-    closeMobileNav();
-    console.log("You haven't clicked anywhere.");
-  }
+document.addEventListener('click', (e) => {
+	if (e.target.matches('#toggle-button, .toggle-button__bar')) {
+		openMobileNav();
+	} else if (e.target.matches('#backdrop')) {
+		closeMobileNav();
+	}
 });
 
 // const toggleButton = document.querySelector(".toggle-button");
